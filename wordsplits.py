83689a1beba1d2ce603385eeb2c8a19a3d934f97
Supraja9726 <suprajabaskaran3@gmail.com>
2022-06-15 17:33:58 -0400

    output without errors

---
+++ wordsplits.py
@@ -0,0 +1,16 @@
+# encoding=utf-8
+import jieba
+import paddle
+
+jieba.enable_paddle()
+
+def words_from_miniapp(miniapp):
+	nickName = miniapp['nickName'].lower()
+	description = miniapp['description'].lower()
+
+	seg_listA = jieba.cut(nickName, cut_all=True)
+	seg_listB = jieba.cut(description, cut_all=True)
+
+	words = set(list(nickName) + list(description) + list(seg_listA) + list(seg_listB))
+
+	return words
